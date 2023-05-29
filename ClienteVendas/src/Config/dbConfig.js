ccf5cf6ec466ec1a0bdc617d1dbd295531ee8b50
aluno <contato.vitorgabrielti@gmail.com>
2023-05-29 10:47:26 -0300

    back full funcional

--- ClienteVendas/src/Config/dbConfig.js
+++ ClienteVendas/src/Config/dbConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-//const dbConfig = "mongodb://0.0.0.0:27017/clientes";
-const dbConfig = "mongodb+srv://VitorGabriel:756482139@cluster0.a6flhdf.mongodb.net/clientes?retryWrites=true&w=majority";
+const dbConfig = "mongodb://0.0.0.0:27017/clientes";
+//const dbConfig = "mongodb+srv://VitorGabriel:756482139@cluster0.a6flhdf.mongodb.net/clientes?retryWrites=true&w=majority";
 const conexao = mongoose.connect(dbConfig,{
     useNewUrlParser:true,
     useUnifiedTopology: true
